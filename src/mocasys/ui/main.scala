d19be6d60e3bf4d152efecdf9c3a6257f4ab6ef0
Tomáš Černý <tmscer@gmail.com>
2019-04-23 15:28:02 +0200

    message on invalid login + styling

--- src/mocasys/ui/main.scala
+++ src/mocasys/ui/main.scala
@@ -5,6 +5,7 @@ import scala.concurrent.ExecutionContext.Implicits.global
 import scalajs.js
 import scalajs.js.annotation._
 import org.scalajs.dom
+import org.scalajs.dom.ext._
 import liwec._
 import liwec.htmlDsl._
 import liwec.htmlMacros._
@@ -15,7 +16,7 @@ package object main {
     def textInput(strValue: String,
                   onChange: String => Unit,
                   typ: String = "text") =
-        input(typeAttr:=typ, value:=strValue, onInput:={
+        input(typeAttr := typ, value := strValue, onInput := {
             e => onChange(e.target.asInstanceOf[dom.raw.HTMLInputElement].value)
         })
 
@@ -23,6 +24,7 @@ package object main {
                     val onLogin: Option[() => Unit] = None)
             extends Component {
         var loginError: String = ""
+
         def render() = scoped(
             div(cls:="loginForm",
                 h2("Login"),
@@ -34,12 +36,22 @@ package object main {
                     textInput(username, { username = _ })),
                 label(span("Password: "),
                     textInput(password, { password = _ }, "password")),
-                button("Login", cls := "submitButton", onClick := { e =>
+                button("Login", cls := "submitButton",  onClick := { e =>
                     if (password.length > 0 && username.length > 0) {
                         AppState.loginWithPassword(username, password)
                         .onComplete {
                             case Success(_) => loginError = "Success!"
-                            case Failure(e) => loginError = s"Failed logging in: $e"
+                            case Failure(e) => {
+                                // Compiler cries without the cast
+                                val response = e.asInstanceOf[AjaxException]
+                                // TODO: Change this once json parsing for errors is done
+                                val json = js.JSON.parse(response.xhr.responseText)
+                                var message = s"Unknown error: ${json.message}"
+                                if (response.xhr.status == 400) {
+                                    message = "Invalid username or password"
+                                }
+                                loginError = message
+                            }
                         }
                     } else {
                         loginError = "Please enter both username and password."
@@ -53,9 +65,20 @@ package object main {
                 display := "grid",
                 gridTemplateColumns := "auto auto",
                 maxWidth := "20em",
+                marginTop := "3em",
+                marginLeft := "auto",
+                marginRight := "auto",
+                backgroundColor := "#eeeeee",
+                padding := "15px",
+                borderRadius := "10px",
 
                 (c.error | e.h2) -> (
                     gridColumn := "1 / 3",
+                ),
+
+                c.error -> (
+                    maxHeight := "3em",
+                    minHeight := "3em",
                 ),
 
                 c.error / e.p -> (
@@ -75,6 +98,7 @@ package object main {
                     gridColumn := "1 / 3",
                     justifySelf := "right",
                     width := "10em",
+                    marginTop := "1em",
                 ),
             )
         }
