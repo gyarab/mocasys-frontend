215d3783dd8f25ebc1519de7f05061c94d0a3bac
Tomáš Černý <tmscer@gmail.com>
2019-04-25 08:59:40 +0200

    food selection mock

---
+++ src/mocasys/ui/pages/foodSelection.scala
@@ -0,0 +1,45 @@
+package mocasys.ui.pages
+
+import scala.util.{Success, Failure}
+import scalajs.js
+import scala.concurrent.ExecutionContext.Implicits.global
+import org.scalajs.dom.ext._
+import liwec._
+import liwec.htmlDsl._
+import liwec.htmlMacros._
+import liwec.cssMacros._
+import liwec.cssDslTypes.RawSelector
+import mocasys._
+import mocasys.ui.components._
+import mocasys.ui.main.textInput
+import mocasys.ui.tables._
+import mocasys.ApiClient._
+
+class FoodSelection extends Component {
+
+    def render = scoped(
+        div(cls := "foodSelection",
+            button("Save", cls := "saveButton", onClick := { e =>
+                println("save!");
+            }),
+
+            new FoodList()
+        )
+    )
+
+    cssScoped { import liwec.cssDsl._
+        c.foodSelection -> (
+            marginLeft := "14em", // Placeholder
+            backgroundColor := "#f3f3f3",
+            height := "100%",
+            padding := "1.5em",
+
+            c.saveButton -> (
+                backgroundColor := "#2196F3",
+                padding := "0.5em 1em",
+                border := "#d3d3d3",
+                borderRadius := "5px",
+            ),
+        )
+    }
+}
