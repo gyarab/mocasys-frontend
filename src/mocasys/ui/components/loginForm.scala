6b140d224f20b1c610cbdda3a6a1755cac2ac579
Tomáš Černý <tmscer@gmail.com>
2019-04-27 23:43:56 +0200

    even more styling of the login page + main sass file

--- src/mocasys/ui/components/loginForm.scala
+++ src/mocasys/ui/components/loginForm.scala
@@ -20,23 +20,25 @@ class LoginForm(var username: String = "", var password: String = "",
     var loginError: String = ""
 
     def render() = scoped(
-        div(cls := "loginForm",
+        div(cls := "loginForm borderRadius",
             div(cls := "error", loginError match {
                 case "" => loginError
-                case _ => span(loginError)
+                case _ => span(cls := "bgRed borderRadius", loginError)
             }),
-            div(cls := "form", 
-                img(cls := "mocasysLogo", src := "/assets/mocasys_logo.svg"),
-                img(cls := "googleLogin", src := "/assets/google_logo.svg"),
+            div(cls := "form bgBlueDarker borderRadius", 
+                img(cls := "mocasysLogo",
+                    src := "/assets/mocasys_logo.svg"),
+                img(cls := "googleLogin borderRadius bgWhite shadowClick",
+                    src := "/assets/google_logo.svg"),
                 label(cls := "username",
-                    span("username"),
+                    span(cls := "borderShadowBlue bgBlue borderRadius", "username"),
                     textInput(username, { username = _ })
                 ),
                 label(cls := "password",
-                    span("password"),
+                    span(cls := "borderShadowBlue bgBlue borderRadius", "password"),
                     textInput(password, { password = _ }, "password")
                 ),
-                button("Log in", cls := "submitButton", onClick := { e =>
+                button("Log in", cls := "submitButton shadowClick", onClick := { e =>
                     if (password.length > 0 && username.length > 0) {
                         AppState.loginWithPassword(username, password)
                         .onComplete {
@@ -79,7 +81,6 @@ class LoginForm(var username: String = "", var password: String = "",
                 gridTemplateRows := "auto 1fr 1fr 20px 1fr 1fr",
                 backgroundColor := "#265976",
                 padding := "20px",
-                borderRadius := "3px",
 
                 c.mocasysLogo -> (
                     gridColumn := "1/4",
@@ -93,14 +94,8 @@ class LoginForm(var username: String = "", var password: String = "",
                     gridRowEnd := "4",
                     width := "3.5em",
                     padding := "0.5em",
-                    backgroundColor := "#f1ffff",
                     justifySelf := "right",
                     marginRight := "12.5%",
-                    borderRadius := "3px",
-                ),
-
-                RawSelector("img:hover") -> (
-                    backgroundColor := "#ff9b20",
                 ),
 
                 c.username -> (
@@ -125,44 +120,31 @@ class LoginForm(var username: String = "", var password: String = "",
                     display := "contents",
 
                     e.input -> (
-                        width := "70%",
+                        width := "75%",
                         height := "3em",
                         gridColumn := "1",
                     ),
 
                     e.span -> (
                         gridColumn := "1",
-                        backgroundColor := "#3685a2",
                         padding := "4px 6px 2px 6px",
-                        borderRadius := "2px",
+                        justifySelf := "left",
+                        marginLeft := "12.5%",
                     ),
                 ),
 
                 c.submitButton (
-                    gridRowStart := "4",
+                    gridRowStart := "5",
                     gridRowEnd := "7",
                     gridColumn := "3",
                     width := "75%",
-                    height :=  "50%",
+                    height :=  "100%",
                     padding := "0",
                     border := "0",
-                    borderRadius := "3px",
                     alignSelf := "center",
-                    background := "#f1ffff",
                     color := "#265976",
                     fontSize := "2em",
                     fontWeight := "400",
-                ),
-
-                RawSelector(".submitButton:hover") -> (
-                    backgroundColor := "#ff9b20",
-                    boxShadow := "7px 7px 50px 0px rgba(0, 0, 0, 0.75)",
-                    transform := "translateY(-2px)",
-                ),
-
-                RawSelector(".submitButton:active") -> (
-                    backgroundColor := "#d23a3f",
-                    transform := "translateY(2px)",
                 ),
             ),
 
@@ -176,10 +158,8 @@ class LoginForm(var username: String = "", var password: String = "",
                 display := "block",
                 marginLeft := "4em",
                 marginRight := "4em",
-                backgroundColor := "#d23a3f",
                 padding := "10px 0px 8px",
                 textAlign := "center",
-                borderRadius := "3px",
             ),
         )
     }
