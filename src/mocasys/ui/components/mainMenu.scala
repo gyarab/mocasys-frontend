cc6d8441187eb660094b5bcff74bcff9b072dd9d
Tomáš Černý <tmscer@gmail.com>
2019-04-28 15:26:09 +0200

    menu hide/show anim + hider shadow

--- src/mocasys/ui/components/mainMenu.scala
+++ src/mocasys/ui/components/mainMenu.scala
@@ -27,17 +27,24 @@ class MainMenu() extends Component {
                     div(cls := "bar1"), 
                     div(cls := "bar2"),
                     div(cls := "bar3"),
+                    onClick := { e => 
+                        val menu = dom.document.querySelector(".mainMenu")
+                            .asInstanceOf[dom.raw.HTMLElement]
+                        if (menu.style.left == "-16%")
+                            menu.style.left = "0"
+                        else menu.style.left = "-16%"
+                    }
                 ),
                 div(cls := "userMenu",
                     span("Logged in as "), b(username.get),
                     button("Profile", cls := "profile bgColor4 shadowClick",
-                    onClick := { e => {
+                    onClick := { e =>
                         dom.window.alert("Not Yet Implemented!")
-                    }}),
+                    }),
                     button("Log Out", cls := "logout bgColor4 shadowClick",
-                    onClick := { e => {
+                    onClick := { e =>
                         dom.window.alert("Not Yet Implemented!")
-                    }}),
+                    }),
                 ),
                 div(cls := "menu",
                 
@@ -47,20 +54,20 @@ class MainMenu() extends Component {
     }
 
     cssScoped { import liwec.cssDsl._
+        // TODO: Fix - on small screen buttons popout
         c.mainMenu -> (
             position := "absolute",
             left := "0",
             top := "0",
             marginTop := "3%",
             width := "16%",
-            minWidth := "10em",
-            maxWidth := "17em",
             height := "90%",
             boxShadow := "5px 5px 10px 0px rgba(0, 0, 0, 0.60)",
             borderBottomLeftRadius := "0",
             borderTopLeftRadius := "0",
             color := "#f1ffff",
             fontFamily := "Helvetica",
+            transition := "left 0.3s ease-in-out",
 
             e.img (
                 width := "100%",
@@ -83,6 +90,7 @@ class MainMenu() extends Component {
                     borderBottomWidth := "0",
                     gridColumn := "1",
                     backgroundColor := "#ff9b20",
+                    boxShadow := "2px 2px 4px 0px rgba(0, 0, 0, 0.60)",
                 ),
 
                 c.bar1 (gridRow := "1"),
@@ -106,12 +114,13 @@ class MainMenu() extends Component {
 
             c.userMenu (
                 display := "grid",
-                gridRowGap := "0.3em",
+                gridRowGap := "0.1em",
                 padding := "0.2em 0.8em 0.4em 0.8em",
 
                 e.b (
                     gridRow := "1",
                     gridColumn := "3",
+                    justifySelf := "right",
                 ),
 
                 e.button (
