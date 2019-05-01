bda5435b6f7d35acb7b1514b75b2f1d7d35acec7
Tomáš Černý <tmscer@gmail.com>
2019-05-01 11:59:14 +0200

    food list fetch using date range, styling

--- src/mocasys/ui/components/food.scala
+++ src/mocasys/ui/components/food.scala
@@ -25,91 +25,61 @@ class Food(
         date.getFullYear() == today.getFullYear()
     }
 
-    def render() = scoped(
-        div(cls := "food" + (if (isToday) " today" else ""),
-            div(cls := "info",
-                span(date.toDateString()),
-                div(cls := "controls",
-                    button("Z"),
-                    button("O"),
-                    button("X"),
-                )
-            ),
-            table(tbody(
-                choices.map(choice => tr(
-                    td(choice("kind").asInstanceOf[String]),
-                    td(choice("name").asInstanceOf[String]),
-                ))
-            )),
+    def render: liwec.VNode = {
+        scoped(
+            div(cls := "food borderRadius" + (if (isToday) " today" else ""),
+                div(cls := "info",
+                    span(date.toDateString()),
+                ),
+                table(tbody(
+                    choices.map(choice => tr(
+                        td(choice("kind").asInstanceOf[String]),
+                        td(choice("name").asInstanceOf[String]),
+                        td(choice("option").asInstanceOf[String]),
+                    ))
+                )),
+            )
         )
-    )
+    }
 
     cssScoped { import liwec.cssDsl._
         c.food -> (
             height := "10em",
             backgroundColor := "white",
-            border := "1px solid #dadce0",
-            borderLeft := "6px solid #56aeB5",
-            borderRadius := "4px",
+            boxShadow := "4px 4px 8px 0px rgba(0, 0, 0, 0.60)",
+            borderTop := "3px solid #3ea7b9",
+            color := "white",
 
             c.info -> (
                 display := "flex",
                 flexDirection := "row",
-                borderBottom := "1px solid #dadce0",
                 height := "31px",
+                backgroundColor := "#265976",
 
                 e.span -> (
                     marginLeft := "0.8em",
-                    marginTop := "0.5em",
-                    marginBottom := "0.2em",
-                ),
-
-                c.controls -> (
-                    marginLeft := "auto",
-
-                    e.button -> (
-                        marginLeft := "10px",
-                        height := "100%",
-                        lineHeight := "100%",
-                        borderRadius := "0",
-                        border := "0px",
-                        fontWeight := "bold",
-                        backgroundColor := "#e69857",
-                    ),
-
-                    RawSelector("button:hover") -> (
-                        backgroundColor := "#e66740",
-                        color := "white",
-                    )
+                    marginTop := "0.4em",
                 ),
             ),
 
             e.table -> (
                 maxWidth := "50%",
+                maxHeight := "100%",
+                marginTop := "0.3em",
                 paddingLeft := "0.8em",
 
                 RawSelector("td:first-child") -> (
                     fontWeight := "bold",
                 ),
-            ),
-        )
 
-        c.today -> (
-            borderLeftColor := "#4caf50",
+                RawSelector("td:nth-child(2)") -> (
+                    paddingLeft := "0.5em",
+                ),
+            ),
         )
 
         c.food /+ c.food -> (
             marginTop := "1em",
-        )
-
-        e.button -> (
-            height := "100%",
-        )
-
-        c.foodEmpty -> (
-            borderLeft := "6px solid grey",
-            color := "#eeeeee",
-            backgroundColor := "#eeeeee",
         )
     }
 }
--- src/mocasys/ui/pages/foodSelection.scala
+++ src/mocasys/ui/pages/foodSelection.scala
@@ -20,11 +20,18 @@ class FoodSelection extends Component {
     var foodList: Option[Seq[DbRow]] = None
     var balance: String = ""
     var error: String = ""
+    var startDate: js.Date = new js.Date()
+    var endDate: js.Date = incrDate(startDate, 6)
+    // Prev dates will be set on food list fetch
+    var prevStartDate: js.Date = incrDate(startDate, -1)
+    var prevEndDate: js.Date = incrDate(endDate, -1)
 
     override def onMount() {
         fetchBalance()
         fetchFoodList()
     }
+
+    def isoDate(date: js.Date) = date.toISOString.substring(0, 10)
 
     // TODO: Replace with query builder
     val balanceQuery = """
@@ -32,7 +39,8 @@ class FoodSelection extends Component {
         WHERE id_person = session_person_get();
         """
     def foodListQuery(start: String, end: String) = s"""
-        SELECT fa.day, fa.id_food, f.name, fa.kind, fc.kind as kind2
+        SELECT fa.day, fa.id_food, f.name, fa.kind,
+            fc.kind as kind2, fa.option, fc.option as option2
         FROM food_assignments AS fa
         LEFT JOIN food_choice AS fc ON fa.day = fc.day AND fa.kind = fc.kind
         JOIN food AS f ON f.id = fa.id_food
@@ -54,7 +62,7 @@ class FoodSelection extends Component {
         }
 
     def fetchFoodList() =
-        AppState.apiClient.queryDb(foodListQuery("2019-04-20", "2019-04-28"))
+        AppState.apiClient.queryDb(foodListQuery(isoDate(startDate), isoDate(endDate)))
         .onComplete {
             case Success(res) => {
                 foodList = Some(res)
@@ -73,13 +81,31 @@ class FoodSelection extends Component {
             .sortBy(_._1))
 
     def render: liwec.VNode = {
-        val start: js.Date = new js.Date()
-        val nDays: Integer = 7
-        val end: js.Date = incrDate(start, nDays - 1)
+        if (startDate != prevStartDate || endDate != prevEndDate) {
+            prevStartDate = startDate
+            prevEndDate = endDate
+            fetchFoodList()
+        }
         return scoped(
             div(cls := "foodSelection",
-                h2(s"${start.toDateString()} - ${end.toDateString()}"),
-                h3("Balance: " + (if (balance == "") "N/A" else balance)),
+                div(cls := "firstRow borderRadius",
+                    label(cls := "dateStart",
+                        span(cls := "borderShadowColor3 bgColor2 borderRadius",
+                            "Date Start"),
+                        textInput(isoDate(startDate),
+                                { str => startDate = new js.Date(str) }, "date"),
+                    ),
+                    label(cls := "dateEnd",
+                        span(cls := "borderShadowColor3 bgColor2 borderRadius",
+                            "Date End"),
+                        textInput(isoDate(endDate),
+                                { str => endDate = new js.Date(str) }, "date"),
+                    ),
+                    label(cls := "balance",
+                        span(cls := "borderShadowColor3 bgColor2 borderRadius balanceLabel", "Balance"),
+                        span(cls := "balanceValue", (if (balance == "") "N/A" else balance)),
+                    )
+                ),
                 div(cls := "error", error),
                 foodByDate.map(fbd =>
                     div(cls := "foodList",
@@ -95,22 +121,45 @@ class FoodSelection extends Component {
     cssScoped { import liwec.cssDsl._
         c.foodSelection -> (
             display := "grid",
+            gridTemplateRows := "auto 2em 100%",
+            gridRowGap := "0.3em",
             backgroundColor := "white",
             height := "100%",
-            padding := "1.5em",
 
-            c.foodList -> (
+            c.foodList (
                 gridColumn := "1 / 4",
+                backgroundColor := "white",
+                padding := "0 1.5em 0 1.5em",
             ),
 
-            e.h2 -> (
-                gridColumnStart := "1",
-                gridColumnEnd := "2",
-                gridRow := "1 / 2",
+            c.firstRow (
+                gridColumn := "1 / 4",
+                display := "flex",
+                flexDirection := "row",
+                backgroundColor := "#265976",
+                padding := "1.5em",
+                color := "white",
+                boxShadow := "5px 5px 10px 0px rgba(0, 0, 0, 0.60)",
+
+                e.span (
+                    padding := "4px 6px 2px 6px",
+                    marginBottom := "0.3em",
+                ),
+
+                c.balanceLabel (textAlign := "right"),
+                c.balanceValue (padding := "10px 7px 7px 7px"),
+
+                e.label (
+                    display := "flex",
+                    flexDirection := "column",
+                    marginRight := "0.5em",
+
+                ),
             ),
 
-            e.h3 -> (
-                gridColumn := "3 / 3",
+            c.balance (
+                gridColumn := "2 / 5",
+                justifySelf := "end",
             ),
         )
     }
